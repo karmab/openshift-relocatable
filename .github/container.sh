4b8a6cd298a511ea35a4245214c22cb8e3f7a752
karmab <karimboumedhel@gmail.com>
2022-11-30 14:56:35 +0100

    add oc and jq to container

--- .github/container.sh
+++ .github/container.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+curl -Ls https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > jq
+curl -k -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz | tar zxf - oc
 podman build -t quay.io/karmab/openshift-relocatable:latest -f Dockerfile .
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 podman push quay.io/karmab/openshift-relocatable:latest
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM quay.io/centos/centos:stream8
 MAINTAINER Karim Boumedhel <karimboumedhel@gmail.com>
+ADD oc /usr/bin/oc
+RUN chmod 700 /usr/bin/oc
+ADD jq /usr/bin/jq
+RUN chmod 700 /usr/bin/jq
 ADD . /root
+RUN dnf -y install gettext openssl podman
 ENTRYPOINT ["/bin/bash", "/root/deploy.sh"]
